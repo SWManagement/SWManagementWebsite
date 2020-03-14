93bc34856230cc45b14f735bd49e4b227986caf1
Martin Strekelj <martin.strekelj123@gmail.com>
2020-03-14 17:55:23 +0100

    first commit

---
+++ Index/index.php
@@ -0,0 +1,4 @@
+<?php echo file_get_contents("indexHeader.html"); ?>
+<?php echo file_get_contents("indexBody.html"); ?>
+<?php echo file_get_contents("indexFooter.html"); ?>
+<p>Copyright © Smart Waste Management <?php echo date("Y"); ?> </p>
