c3e11f708a2703ff486c513f7dc96baf915d060f
Martin Strekelj <martin.strekelj123@gmail.com>
2020-03-14 21:13:45 +0100

    Added mobile view, o ekipi, and footer style

--- Index/team.php
+++ Index/team.php
@@ -1,3 +1,3 @@
 <?php echo file_get_contents("header.html"); ?>
-<?php echo file_get_contents("projectInfoBody.html"); ?>
+<?php echo file_get_contents("teamInfoBody.html"); ?>
 <?php echo file_get_contents("footer.html"); ?>
